9ae768b878e08c4cfa5631cc3b73ec89a6e089cc
Sara Salah <sarasallah165091@gmail.com>
2025-05-07 10:21:03 +0300

    Enable django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -8,20 +8,21 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager(BaseUserManager):
     """ Manager for user profiles """
-    def create_user(self , name , password =None):
+    def create_user(self ,email , name , password =None):
         """ Create a new user profile """
         if not email:
             raise ValueError("User must have an email")
         email = self.normalize_email(email)
+
         user = self.model(email=email , name=name)
 
         user.set_password(password)
-        user.model(using=self._db)
+        user.save(using=self._db)
 
-        return User
+        return user
 
     def create_superuser(self , email , name, password):
-        """ Create and Sava a new superuser with """
+        """ Create and save a new superuser """
         user = self.create_user(email, name ,password)
 
         user.is_superuser =True
@@ -35,14 +36,14 @@ class UserProfile(AbstractBaseUser , PermissionsMixin):
     """ Database model for user in the system """
     email =  models.EmailField(max_length=255 , unique=True)
     name = models.CharField(max_length=255)
-    is_Active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = [ 'name' ]
+    REQUIRED_FIELDS = [ 'name' ]
 
     def get_full_name(self):
         """ Retrieve full name of user """
